37743c60ab0bd18effe6612cbb627fb3a622902a
MullayanovRustam <rustam.mullayanov@gmail.com>
2021-11-10 22:00:11 +0500

    добавил суффикс UseCase юзкейсам

--- app/src/main/java/com/example/tinkoff_hr/domain/usecases/UpdateWorkerByEmailUseCase.kt
+++ app/src/main/java/com/example/tinkoff_hr/domain/usecases/UpdateWorkerByEmailUseCase.kt
@@ -3,7 +3,7 @@ package com.example.tinkoff_hr.domain.usecases
 import com.example.tinkoff_hr.domain.entities.Worker
 import com.example.tinkoff_hr.domain.repositories_interface.WorkerRepository
 
-class UpdateWorkerByEmail(private val workerRepository: WorkerRepository) {
+class UpdateWorkerByEmailUseCase(private val workerRepository: WorkerRepository) {
     operator fun invoke(worker: Worker): Boolean {
         return workerRepository.updateWorkerInfo(worker);
     }
--- app/src/main/java/com/example/tinkoff_hr/presentation/ProfilePresenter.kt
+++ app/src/main/java/com/example/tinkoff_hr/presentation/ProfilePresenter.kt
@@ -2,13 +2,13 @@ package com.example.tinkoff_hr.presentation
 
 import com.example.tinkoff_hr.data.repositories.WorkerRepositoryImpl
 import com.example.tinkoff_hr.domain.entities.Worker
-import com.example.tinkoff_hr.domain.usecases.GetWorkerInfoByEmail
-import com.example.tinkoff_hr.domain.usecases.UpdateWorkerByEmail
+import com.example.tinkoff_hr.domain.usecases.GetWorkerInfoByEmailUseCase
+import com.example.tinkoff_hr.domain.usecases.UpdateWorkerByEmailUseCase
 
 class ProfilePresenter{
     private val workerRepository = WorkerRepositoryImpl()
-    private val getWorkerInfoByEmail = GetWorkerInfoByEmail(workerRepository)
-    private val updateWorkerByEmail = UpdateWorkerByEmail(workerRepository)
+    private val getWorkerInfoByEmail = GetWorkerInfoByEmailUseCase(workerRepository)
+    private val updateWorkerByEmail = UpdateWorkerByEmailUseCase(workerRepository)
 
     fun onAppearing(email: String){
         getWorkerInfoByEmail(email)
