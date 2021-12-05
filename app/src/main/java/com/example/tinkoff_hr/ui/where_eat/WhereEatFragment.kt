8bc283a2dab6b7418be687a52bdde3459b97baad
Andrew Krysh <vip.krysh@mail.ru>
2021-12-05 13:25:53 +0500

    добавил карту

--- app/src/main/java/com/example/tinkoff_hr/ui/where_eat/WhereEatFragment.kt
+++ app/src/main/java/com/example/tinkoff_hr/ui/where_eat/WhereEatFragment.kt
@@ -5,11 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.tinkoff_hr.R
 import com.example.tinkoff_hr.databinding.FragmentWhereEatBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
-class WhereEatFragment : Fragment() {
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+
+
+class WhereEatFragment : Fragment() , OnMapReadyCallback{
 
     private var _binding: FragmentWhereEatBinding? = null
+    private lateinit var googleMap: GoogleMap
+    private var mapReady = false
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,11 +36,20 @@ class WhereEatFragment : Fragment() {
         _binding = FragmentWhereEatBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+       // binding.mapWhereEat.onStart()
+       // val mapFragment = childFragmentManager.findFragmentById(R.id.map_where_eat) as SupportMapFragment
+
         return root
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
     }
 }
