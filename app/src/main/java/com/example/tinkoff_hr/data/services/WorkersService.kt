162d10a7b5c19909a009cd7c3ae4cf55c9fde72f
MullayanovRustam <rustam.mullayanov@gmail.com>
2021-12-07 00:24:54 +0500

    добавил параметр запроса email в метод getWorkerByEmail WorkersService

--- app/src/main/java/com/example/tinkoff_hr/data/services/WorkersService.kt
+++ app/src/main/java/com/example/tinkoff_hr/data/services/WorkersService.kt
@@ -4,11 +4,12 @@ import com.example.tinkoff_hr.data.entities.WorkerEntityForApi
 import com.example.tinkoff_hr.data.entities.WorkersEntityForApi
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WorkersService {
     @GET("/api/workers")
     fun getWorkers() : Single<WorkersEntityForApi>
 
-    @GET("/api/worker/:email")
-    fun getWorkerByEmail() : Single<WorkerEntityForApi>
+    @GET("/api/workers")
+    fun getWorkerByEmail(@Query("email") email: String) : Single<WorkerEntityForApi>
 }
