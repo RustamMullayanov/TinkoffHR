4517c4530aaabb14eab73daaf5e61688ac0ce481
Andrew Krysh <vip.krysh@mail.ru>
2021-12-06 16:19:54 +0500

    добавил боттом шит и ресайкл в нём

---
+++ app/src/main/java/com/example/tinkoff_hr/ui/where_eat/Eatery.kt
@@ -0,0 +1,3 @@
+package com.example.tinkoff_hr.ui.where_eat
+
+class Eatery (val rating: Double, val name: String)
--- app/src/main/java/com/example/tinkoff_hr/ui/where_eat/WhereEatFragment.kt
+++ app/src/main/java/com/example/tinkoff_hr/ui/where_eat/WhereEatFragment.kt
@@ -5,23 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.tinkoff_hr.R
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tinkoff_hr.databinding.FragmentWhereEatBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
-
-
-class WhereEatFragment : Fragment() , OnMapReadyCallback{
+class WhereEatFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentWhereEatBinding? = null
+    private val eateryAdapter = EateryAdapter()
     private lateinit var googleMap: GoogleMap
-    private var mapReady = false
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -36,12 +31,58 @@ class WhereEatFragment : Fragment() , OnMapReadyCallback{
         _binding = FragmentWhereEatBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-       // binding.mapWhereEat.onStart()
-       // val mapFragment = childFragmentManager.findFragmentById(R.id.map_where_eat) as SupportMapFragment
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.sheet.bottomSheetWhereEat)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
 
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN)
+                    binding.btnExpend.show()
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+                    binding.btnExpend.show()
+                }
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+            }
+        })
+
+
+        binding.sheet.recEatery.apply {
+            layoutManager = LinearLayoutManager(this.context)
+            adapter = eateryAdapter
+        }
+
+        binding.btnExpend.setOnClickListener {
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_HALF_EXPANDED
+            binding.btnExpend.hide()
+        }
+
+
+
+        eateryAdapter.addList(
+            listOf(
+                Eatery(6.0, "Рада"),
+                Eatery(5.3, "А ты где?"),
+                Eatery(8.1, "Ситора"),
+                Eatery(8.0, "Японамама"),
+                Eatery(7.5, "Рататуй"),
+                Eatery(9.3, "Пан Пицца"),
+                Eatery(8.2, "KFC"),
+                Eatery(6.0, "Рада"),
+                Eatery(5.3, "А ты где?"),
+                Eatery(8.1, "Ситора"),
+                Eatery(8.0, "Японамама"),
+                Eatery(7.5, "Рататуй"),
+                Eatery(9.3, "Пан Пицца"),
+                Eatery(8.2, "KFC"),
+            )
+        )
         return root
     }
-
 
 
     override fun onDestroyView() {
