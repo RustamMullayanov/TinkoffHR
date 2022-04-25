1623fd9653bbf8c1ce49ca6e6ae9ed30ab10f7a6
MullayanovRustam <rustam.mullayanov@gmail.com>
2022-04-26 02:36:27 +0500

    Доработал сущность RestaurantReviewEntityForDB

--- app/src/main/java/com/example/tinkoff_hr/data/entities/restaurant/RestaurantReviewEntityForDB.kt
+++ app/src/main/java/com/example/tinkoff_hr/data/entities/restaurant/RestaurantReviewEntityForDB.kt
@@ -1,12 +1,33 @@
 package com.example.tinkoff_hr.data.entities.restaurant
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "restaurant_reviews")
 class RestaurantReviewEntityForDB(
+
+    @PrimaryKey @ColumnInfo(name = "review_id")
     val id: String,
+
+    @ColumnInfo(name = "worker_id")
     val workerId: String,
+
+    @ColumnInfo(name = "restaurant_id")
     val restaurantId: String,
+
+    @ColumnInfo(name = "review_rating")
     val rating: Double,
+
+    @ColumnInfo(name = "review_text")
     val text: String,
-    val workerName: String? = null,
-    val workerSurname: String? = null,
-    val workerPatronymic: String? = null
+
+    @ColumnInfo(name = "worker_name")
+    val workerName: String,
+
+    @ColumnInfo(name = "worker_surname")
+    val workerSurname: String,
+
+    @ColumnInfo(name = "worker_patronymic")
+    val workerPatronymic: String
 )
