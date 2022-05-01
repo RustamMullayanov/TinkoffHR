a47e1b2962887577f01b1ef963f8c883318b4743
Andrew Krysh <vip.krysh@mail.ru>
2022-05-01 22:51:58 +0500

    исправил новые конфликты

--- app/src/main/java/com/example/tinkoff_hr/ui/where_eat/eatery_information/ReviewEateryAdapter.kt
+++ app/src/main/java/com/example/tinkoff_hr/ui/where_eat/eatery_information/ReviewEateryAdapter.kt
@@ -1,52 +1,29 @@
 package com.example.tinkoff_hr.ui.where_eat.eatery_information
 
 import android.annotation.SuppressLint
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.example.tinkoff_hr.R
-import com.example.tinkoff_hr.databinding.CardReviewEateryBinding
 import com.example.tinkoff_hr.domain.entities.restaurant.RestaurantReview
+import com.example.tinkoff_hr.ui.tribute.item.BaseListItem
+import com.hannesdorfmann.adapterdelegates4.ListDelegationAdapter
+import com.hannesdorfmann.adapterdelegates4.dsl.adapterDelegate
 
 
-class ReviewEateryAdapter : RecyclerView.Adapter<ReviewEateryAdapter.ReviewEateryHolder>() {
+class ReviewEateryAdapter : ListDelegationAdapter<List<BaseListItem>>(){
 
-    private var reviews: List<RestaurantReview> = emptyList()
-
-    class ReviewEateryHolder(val viewBinding: CardReviewEateryBinding) :
-        RecyclerView.ViewHolder(viewBinding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewEateryHolder {
-        val binding =
-            CardReviewEateryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return ReviewEateryHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: ReviewEateryHolder, position: Int) {
-        val item = reviews[position]
-        with(holder.viewBinding) {
-            reviewerName.text =
-                "${item.workerSurname} ${item.workerName} ${item.workerPatronymic ?: ""}"
-            reviewText.text = item.text
-            reviewerPhoto.setImageResource(R.drawable.ic_account_circle_24)
+    init {
+        delegatesManager.apply {
+            addDelegate(ReviewEateryDelegateProvider.provideDelegate())
         }
-
-    }
-
-    override fun getItemCount(): Int {
-        return reviews.size
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    fun setList(list: List<RestaurantReview>) {
-        reviews = list
+    fun setNewItems(newItems: List<BaseListItem>) {
+        items = newItems
         notifyDataSetChanged()
     }
 
     @SuppressLint("NotifyItemChanged")
-    fun setReview(review: RestaurantReview) {
-        reviews = reviews + listOf(review)
-        notifyItemChanged(reviews.lastIndex)
+    fun setItem(item: BaseListItem) {
+        items = items?.plus(item)
+        notifyItemChanged(itemCount)
     }
 }
